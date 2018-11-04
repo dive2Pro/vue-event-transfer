aa2d2d526495dea9fe55d76f8b0cee51d8940109
1c <toylucklebo@gmail.com>
2018-11-04 15:32:56 +0800

    update with rollup

---
+++ dist/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function index(_Vue, options) {
+  let name = "e-transfer";
+  if (options && options.name) {
+    name = options.name;
+  }
+
+  _Vue.directive(name, function(el, bindling, $vnode) {
+    const { arg, value, expression } = bindling;
+    const { context, componentInstance: instance } = $vnode;
+    const origin$emit = instance.$emit;
+    const { _events } = instance;
+    instance.$emit = function(name, ...args) {
+      if (_events[name]) {
+        return origin$emit(name, ...args);
+      }
+      const changedName = arg ? arg + "-" + name : name;
+      context.$emit(changedName, ...args, value);
+    };
+  });
+}
+
+module.exports = index;
